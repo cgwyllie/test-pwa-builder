2d02f7c2ac8c815491cb7dd362a2e07fa3e1fe01
Christopher Wyllie <chris_w@optimove.com>
2023-05-11 14:45:39 +0100

    Add test SDK integration

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,10 @@
 importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js'
+  'https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js'
 );
 
 // This is your Service Worker, you can put any of your custom Service Worker
 // code in this file, above the `precacheAndRoute` line.
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+
+importScripts('https://static.app.delivery/sdks/web/optimove-worker.js');
